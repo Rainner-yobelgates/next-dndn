f13b06c5c3b50db50065ef6617a048016452c827
ynrrizki <yanuarrizki165@gmail.com>
2024-08-29 05:30:33 +0700

    add: illustration empty cart in cart page

--- src/app/(home)/cart/page.tsx
+++ src/app/(home)/cart/page.tsx
@@ -2,6 +2,7 @@
 
 import CartItem from "@/components/cart/CartItem"
 import Summary from "@/components/cart/Summary"
+import { Icons } from "@/components/Icons"
 import CartSkeleton from "@/components/skeleton/CartSkeleton"
 import useCart from "@/hooks/useCart"
 import Head from "next/head"
@@ -29,7 +30,15 @@ const page = () => {
                 <div className='mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12'>                    
                     <div className='lg:col-span-7'>
                         {cart.items.length === 0 && (
-                            <p className='text-neutral-500'>No items added to cart</p>
+                            // <p className='text-neutral-500'>No items added to cart</p>
+                            <div>
+                                <div className='flex items-center justify-center'>
+                                    <Icons.emptyCart size={100} />
+                                </div>
+                                <p className='text-center text-lg text-muted-foreground mt-4'>
+                                    Keranjang kamu masih kosong, yuk belanja dulu!!
+                                </p>
+                            </div>
                         )}
                         <ul>
                             {cart.getItems().map((item) => (
